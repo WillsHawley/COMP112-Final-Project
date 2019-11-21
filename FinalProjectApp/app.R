f03caae923a671a96a5db09028471d7ec68746a8
jacobkres <jkresnic@macalester.edu>
2019-11-21 15:52:46 -0600

    Made some changes to the app

--- FinalProjectApp/app.R
+++ FinalProjectApp/app.R
@@ -3,11 +3,26 @@ library(shiny)
 library(tidyverse)
 
 ui <- fluidPage(
-    titlePanel("Spotify Data App"))
+    titlePanel("Spotify Data App"),
+    selectInput(inputId = "genre", label = "Genre of Music",
+                choices = list("Pop", "Rap", "Rock", "Hip-Hop", "Dance", "Indie",
+                               "Children's Music", "R&B", "Alternative",
+                               "Folk", "Soul", "Country", "Jazz", "Electronic",
+                               "Reggaeton", "Reggae", "World", "Blues",
+                               "Soundtrack", "Classical", "Ska", "Anime",
+                               "Comedy", "Opera", "Movie", "A Capella")),
+    selectInput(inputId = "xaxis", label = "X-Variable", 
+                choices = list("valence", "danceability")),
+    plotOutput(outputId = "spotplot"))
 
 
 server <- function(input, output){
-    ({})
+  output$spotplot <- renderPlot({
+    Spotify_Big %>%
+      filter(genre == input$genre) %>%
+      ggplot() +
+      geom_bin2d(aes(y=popularity, x= input$xaxis))
+    })
 }
 
 
